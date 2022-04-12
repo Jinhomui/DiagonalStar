35f494c4017222d04842f2e128fadb3927261241
Christian Mui <jinhomui27@gmail.com>
2022-04-11 23:35:29 -0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printSquareStar(8);
     }
 
     public static String printSquareStar(int number){
         if (number<5){
             System.out.println("Invalid Value");
+
             return "Invalid Value";
         }
 
-        
+
 
     }
 
