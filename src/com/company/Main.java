4fd678557da92e0aae6abdec29c525df06bb7979
Christian Mui <jinhomui27@gmail.com>
2022-04-12 22:01:40 -0700

    added code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        printSquareStar(8);
+        printSquareStar(5);
     }
 
     public static String printSquareStar(int number){
         if (number<5){
             System.out.println("Invalid Value");
-
             return "Invalid Value";
         }
 
+        int count = 5;
 
+        System.out.printf("*****");
+        while(number>count){
+            count++;
+            System.out.printf("*");
+        }
+        while ()
+
+
+        System.out.println(count);
+
+        return "Test";
 
     }
-
 
 }
